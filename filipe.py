88372fdb5dfd63440acb05775933a52f884cff4e
lipecarmo <lipeoliveira523@gmail.com>
2024-10-14 19:35:30 -0300

    Add files via upload

---
+++ filipe.py
@@ -0,0 +1,33 @@
+# idade= 19
+# print(idade)
+
+# altura= 1.73
+# print(altura)
+
+# nome='filipe'
+# print(nome)
+
+# solteiro=True
+# print(solteiro)
+
+# nome= input ('Digite seu nome:')
+# print(nome)
+
+# print(9.24)
+
+# Bimestre1=int(input('Digite a primeira nota:'))
+
+# Bimestre2=int(input('Digite a segunda nota:'))
+
+# Bimestre3=int(input('Digite a terceira nota:'))
+
+# Bimestre4=int(input('Digite a quarta nota:'))
+
+# media=(Bimestre1+Bimestre2+Bimestre3+Bimestre4)/4
+# print(media)
+
+
+# Salario=float(input("Digite seu salário"))
+# Horas=float(input('Digite quantas horas trabalha no mes'))
+
+
